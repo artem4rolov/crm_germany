312fd6797878d25b6a307627cda81f1d80a9a3ae
Artem <artem4rolov@gmail.com>
2023-05-08 22:11:33 +0300

    new month label

--- src/tabs/Timesheet/Timesheet.jsx
+++ src/tabs/Timesheet/Timesheet.jsx
@@ -40,6 +40,18 @@ const Styles = styled.div`
       border-color: #fcfcfc;
     }
 
+    .month_name {
+      background-color: #4f6780;
+      color: #ffffff;
+      /* border-radius: 8px; */
+      width: 100%;
+      border: 1px solid #4f6780;
+
+      th {
+        border: 1px solid #4f6780;
+      }
+    }
+
     .table-content {
       position: relative;
 
@@ -117,11 +129,9 @@ const Timesheet = () => {
   const dispatch = useDispatch();
 
   // достаем переменные из стейта timesheet
-  const { filterDateTimesheet, needRefreshData } = useSelector(
-    (state) => state.timesheet
-  );
+  const { filterDateTimesheet } = useSelector((state) => state.timesheet);
   // достаем переменные из стейта holidays
-  const { holidays } = useSelector((state) => state.holidays);
+  const { holidays, needRefreshData } = useSelector((state) => state.holidays);
 
   // массив дней, который мы будем выводить
   const [tableDays, setTableDays] = useState(null);
@@ -188,28 +198,27 @@ const Timesheet = () => {
 
   // console.log(data);
 
-  useEffect(() => {
-    if (holidays && holidays.length > 0 && tableDays) {
-      getRangeArray();
-    }
-  }, [filterDateTimesheet]);
-
   // при первом рендере получаем ВСЕ праздники с  сервера
   useEffect(() => {
-    const promise = new Promise((resolve, reject) => {
-      const actualyYear = new Date().getFullYear();
-      dispatch(
-        getHolidaysByFilter({
-          date: `01.01.2001-31.12.${actualyYear}`,
-          region: null,
-        })
-      );
-    });
-    if (holidays && holidays.length > 0 && !tableDays) {
-      getRangeArray();
-    }
+    const actualyYear = new Date().getFullYear();
+    dispatch(
+      getHolidaysByFilter({
+        date: `01.01.2010-31.12.${actualyYear}`,
+        region: null,
+      })
+    );
+
+    // if (holidays && holidays.length > 0 && !tableDays) {
+    //   getRangeArray();
+    // }
     return () => {};
   }, []);
+
+  useEffect(() => {
+    if (holidays && holidays.length > 0) {
+      getRangeArray();
+    }
+  }, [filterDateTimesheet, holidays]);
 
   return (
     <Styles>
@@ -238,71 +247,92 @@ const Timesheet = () => {
               {tableDays &&
                 tableDays.map((row, index) =>
                   !row.holiday ? (
-                    <tr key={index} className={`table-content`}>
-                      <th>{index + 1}</th>
-                      <th>{moment(row._d).format("DD.MM.YY")}</th>
-                      <th>
-                        {row.project && row.project.name
-                          ? row.project.name
-                          : ""}
-                      </th>
-                      <th>
-                        {row.project && row.project.von ? row.project.von : ""}
-                      </th>
-                      <th>
-                        {row.project && row.project.bis ? row.project.bis : ""}
-                      </th>
-                      <th>
-                        {row.project && row.project.pause
-                          ? row.project.pause
-                          : ""}
-                      </th>
-                      <th>
-                        {row.project && row.project.zeit
-                          ? row.project.zeit
-                          : ""}
-                      </th>
-                      <th>
-                        {row.project && row.project.pt ? row.project.pt : ""}
-                      </th>
-                      <th>
-                        {row.project && row.project.note
-                          ? row.project.note
-                          : ""}
-                      </th>
-                      {/* модалка в углу строки при клике на строку */}
-                      <th className="row-modal">
-                        <div>
-                          <img
-                            src={PlusIcon}
-                            alt="plus icon"
-                            onClick={() =>
-                              setToggleAddProjectToday((prev) => !prev)
-                            }
-                          />
-                        </div>
-                        <div>
-                          <img
-                            src={EditIcon}
-                            alt="edit icon"
-                            onClick={() => {
-                              setCurrentProject(row);
-                              setToggleEditProjectToday((prev) => !prev);
-                            }}
-                          />
-                        </div>
-                        <div>
-                          <img
-                            src={TrashIcon}
-                            alt="trash icon"
-                            onClick={() => {
-                              setCurrentProject(row[2]);
-                              setToggleRemoveProjectToday((prev) => !prev);
-                            }}
-                          />
-                        </div>
-                      </th>
-                    </tr>
+                    <>
+                      <tr key={index} className={`table-content`}>
+                        <th>{index + 1}</th>
+                        <th>{moment(row._d).format("DD.MM.YY")}</th>
+                        <th>
+                          {row.project && row.project.name
+                            ? row.project.name
+                            : ""}
+                        </th>
+                        <th>
+                          {row.project && row.project.von
+                            ? row.project.von
+                            : ""}
+                        </th>
+                        <th>
+                          {row.project && row.project.bis
+                            ? row.project.bis
+                            : ""}
+                        </th>
+                        <th>
+                          {row.project && row.project.pause
+                            ? row.project.pause
+                            : ""}
+                        </th>
+                        <th>
+                          {row.project && row.project.zeit
+                            ? row.project.zeit
+                            : ""}
+                        </th>
+                        <th>
+                          {row.project && row.project.pt ? row.project.pt : ""}
+                        </th>
+                        <th>
+                          {row.project && row.project.note
+                            ? row.project.note
+                            : ""}
+                        </th>
+                        {/* модалка в углу строки при клике на строку */}
+                        <th className="row-modal">
+                          <div>
+                            <img
+                              src={PlusIcon}
+                              alt="plus icon"
+                              onClick={() =>
+                                setToggleAddProjectToday((prev) => !prev)
+                              }
+                            />
+                          </div>
+                          <div>
+                            <img
+                              src={EditIcon}
+                              alt="edit icon"
+                              onClick={() => {
+                                setCurrentProject(row);
+                                setToggleEditProjectToday((prev) => !prev);
+                              }}
+                            />
+                          </div>
+                          <div>
+                            <img
+                              src={TrashIcon}
+                              alt="trash icon"
+                              onClick={() => {
+                                setCurrentProject(row[2]);
+                                setToggleRemoveProjectToday((prev) => !prev);
+                              }}
+                            />
+                          </div>
+                        </th>
+                      </tr>
+
+                      {moment(row._d).month() !==
+                      moment(row._d).add(1, "day").month() ? (
+                        <tr className="month_name">
+                          <th>{moment(row._d).add(1, "day").format("MMMM")}</th>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                          <th>Arbeitstage: </th>
+                          <th>Feiertage: </th>
+                        </tr>
+                      ) : null}
+                    </>
                   ) : (
                     <tr key={index} className={`table-content holiday`}>
                       <th>{index + 1}</th>
